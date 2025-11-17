c8161176b083fc426057cad605db9c76c76670c3
ChrisFraserNJIT <cf286@njit.edu>
2025-11-17 18:02:16 -0500

    Added default files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,7 +6,7 @@ This package contains all SQLAlchemy ORM models for the application.
 Models define the structure of database tables and relationships between them.
 """
 
-from app.models.user import User
+from app.models.user import User # Using app models folder
 from app.models.calculation import (
     Calculation,
     Addition,
